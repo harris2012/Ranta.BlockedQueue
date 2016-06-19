c57e55921a70dd23b4f29630388bdd2371e30bb7
HarrisZhang <harriszhang@live.cn>
2016-06-19 15:28:26 +0800

    init project

---
+++ Ranta.BlockedQueue/BQueue.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ranta.BlockedQueue
+{
+    /// <summary>
+    /// 提供线程安全的阻塞队列
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    internal class BQueue<T>
+    {
+        private Queue<T> queue;
+
+        private object lockObject;
+
+        public const int MaxSize = 5;
+
+        public BQueue()
+        {
+            this.queue = new Queue<T>();
+
+            this.lockObject = new object();
+        }
+
+        public void Enqueue(T t)
+        {
+            lock (lockObject)
+            {
+                while (this.queue.Count == 5)
+                {
+                    Console.WriteLine("队列已满，等待中");
+
+                    Monitor.Wait(lockObject, 500);
+                }
+                this.queue.Enqueue(t);
+                Monitor.PulseAll(lockObject);
+            }
+        }
+
+        public T Dequeue()
+        {
+            lock (lockObject)
+            {
+                while (this.Count == 0)
+                {
+                    Console.WriteLine("队列为空，等待中");
+
+                    Monitor.Wait(lockObject, 500);
+                }
+                var t = this.queue.Dequeue();
+                Monitor.PulseAll(lockObject);
+                return t;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return this.queue.Count;
+            }
+        }
+
+    }
+}
---
+++ Ranta.BlockedQueue/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ranta.BlockedQueue
+{
+    class Program
+    {
+        static BQueue<string> queue;
+
+        static void Main(string[] args)
+        {
+            queue = new BQueue<string>();
+
+            var tasks = new List<Task>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                tasks.Add(new Task(() =>
+                {
+                    Request();
+                }));
+                tasks.Add(new Task(() =>
+                {
+                    Process();
+                }));
+            }
+
+            foreach (var task in tasks)
+            {
+                task.Start();
+            }
+
+            Task.WaitAll(tasks.ToArray());
+
+            Console.WriteLine("Press any key to continue.");
+            Console.Read();
+        }
+
+        static int requestIndex = 1;
+        static int processIndex = 1;
+
+        static Random random = new Random();
+
+        static void Request()
+        {
+            var current = requestIndex++;
+
+            var message = string.Format("This is test {0}.", current);
+
+            Console.WriteLine("正在请求:{0}", message);
+
+            Thread.Sleep(50 * random.Next(5, 10));
+
+            queue.Enqueue(message);
+
+            Console.WriteLine("请求成功");
+        }
+
+        static void Process()
+        {
+            var current = processIndex++;
+
+            Console.WriteLine("即将处理");
+
+            Thread.Sleep(300 * random.Next(5, 10));
+
+            var t = queue.Dequeue();
+
+            Console.WriteLine("正在处理{0}", t);
+
+            Console.WriteLine("处理成功");
+        }
+    }
+}
